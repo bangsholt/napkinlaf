b635ee636873c4520e9134500136516e4dfb2a52
kcrca <kcrca@494c9712-1f0f-0410-9e33-c2b6180fd7fb>
2004-05-11 05:20:10 +0000

    Progress bar

--- trunk/napkin/src/napkin/NapkinProgressBarUI.java
+++ trunk/napkin/src/napkin/NapkinProgressBarUI.java
@@ -8,6 +8,8 @@ import javax.swing.plaf.*;
 import javax.swing.plaf.basic.*;
 
 public class NapkinProgressBarUI extends BasicProgressBarUI {
+    private final ScribbleHolder scribble = new ScribbleHolder();
+    private final Rectangle sz = new Rectangle(0, 0, 0, 0);
 
     public static ComponentUI createUI(JComponent c) {
         return NapkinUtil.uiFor(c, new NapkinProgressBarUI());
@@ -20,7 +22,31 @@ public class NapkinProgressBarUI extends BasicProgressBarUI {
 
     public void paint(Graphics g, JComponent c) {
         NapkinUtil.defaultGraphics(g);
+        c.setForeground(Color.black);
         super.paint(g, c);
+    }
+
+    protected void paintDeterminate(Graphics g, JComponent c) {
+        Insets b = progressBar.getInsets(); // area for border
+        int barRectWidth = progressBar.getWidth() - (b.right + b.left);
+        int barRectHeight = progressBar.getHeight() - (b.top + b.bottom);
+
+        // amount of progress to draw
+        int amountFull = getAmountFull(b, barRectWidth, barRectHeight);
+
+        c.getBounds(sz);
+        int orientation = progressBar.getOrientation();
+        boolean backwards = !NapkinUtil.isLeftToRight(c);
+        scribble.shapeUpToDate(c, sz, orientation, amountFull, backwards);
+
+        g.setColor(NapkinIconFactory.CheckBoxIcon.MARK_COLOR);
+        scribble.draw(g);
+
+        // Deal with possible text painting
+        if (progressBar.isStringPainted()) {
+            paintString(g, b.left, b.top, barRectWidth, barRectHeight,
+                    amountFull, b);
+        }
     }
 
     public void update(Graphics g, JComponent c) {
---
+++ trunk/napkin/src/napkin/ScribbleHolder.java
@@ -0,0 +1,60 @@
+
+package napkin;
+
+import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
+class ScribbleHolder extends ShapeHolder implements NapkinConstants {
+    private Rectangle size;
+    private Insets insets;
+    private int orientation;
+    private int shown;
+    private boolean backwards;
+
+    private static final float LINE_WIDTH = 3;
+
+    ScribbleHolder() {
+        super(new ScribbleGenerator(LINE_WIDTH), LINE_WIDTH);
+    }
+
+    void shapeUpToDate(Component c, Rectangle sz, int orient, int shn,
+            boolean bwrds) {
+        Insets in = (c instanceof JComponent ?
+                ((JComponent) c).getInsets() : DrawnBorder.DEFAULT_INSETS);
+
+        if (size != null && bwrds == backwards && insets.equals(in) &&
+                orientation == orient && shown == shn &&
+                size.width == sz.width && size.height == sz.height) {
+
+            return;
+        }
+
+        size = (Rectangle) sz.clone();
+        insets = (Insets) in.clone();
+        orientation = orient;
+        shown = shn;
+        backwards = bwrds;
+
+        int cornerX = in.top / 2 + 1;
+        int cornerY = in.left / 2 + 1;
+
+        double innerWidth = sz.getWidth() - (in.left + in.right);
+        double innerHeight = sz.getHeight() - (in.top + in.bottom);
+
+        ScribbleGenerator gen = (ScribbleGenerator) this.gen;
+        gen.setShown(shown);
+        gen.setOrientation(orientation);
+        gen.setRange(orientation == HORIZONTAL ? innerHeight : innerWidth);
+        gen.setMax(orientation == HORIZONTAL ?  innerWidth: innerHeight);
+
+        AffineTransform matrix = new AffineTransform();
+        matrix.translate(cornerY, cornerX);
+        if (backwards) {
+            matrix.translate(size.getWidth(), size.getHeight());
+            matrix.scale(-1, -1);
+        }
+
+        shape = gen.generate(matrix);
+    }
+}
