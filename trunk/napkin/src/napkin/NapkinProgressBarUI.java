437422bee274bad7fefdd55faf7571f36a646a25
kcrca <kcrca@494c9712-1f0f-0410-9e33-c2b6180fd7fb>
2004-05-17 03:18:38 +0000

    Handle indeterminate progress bar

--- trunk/napkin/src/napkin/NapkinProgressBarUI.java
+++ trunk/napkin/src/napkin/NapkinProgressBarUI.java
@@ -10,6 +10,8 @@ import javax.swing.plaf.basic.*;
 public class NapkinProgressBarUI extends BasicProgressBarUI {
     private final ScribbleHolder scribble = new ScribbleHolder();
     private final Rectangle sz = new Rectangle(0, 0, 0, 0);
+    private Rectangle boxRect;
+    private BoxHolder box;
 
     public static ComponentUI createUI(JComponent c) {
         return NapkinUtil.uiFor(c, new NapkinProgressBarUI());
@@ -24,6 +26,40 @@ public class NapkinProgressBarUI extends BasicProgressBarUI {
         NapkinUtil.defaultGraphics(g);
         c.setForeground(Color.black);
         super.paint(g, c);
+    }
+
+    protected void paintIndeterminate(Graphics g1, JComponent c) {
+        Insets b = progressBar.getInsets(); // area for border
+        int barRectWidth = progressBar.getWidth() - (b.right + b.left);
+        int barRectHeight = progressBar.getHeight() - (b.top + b.bottom);
+
+        Graphics2D g = (Graphics2D) g1;
+
+        // Paint the bouncing box.
+        boxRect = getBox(boxRect);
+        if (boxRect == null)
+            return;
+
+        if (box == null) {
+            box = new BoxHolder();
+            box.width = 2;
+        }
+        box.shapeUpToDate(null, boxRect);
+        Graphics2D lineG = NapkinUtil.copy(g);
+        lineG.setColor(NapkinIconFactory.CheckBoxIcon.MARK_COLOR);
+        lineG.translate(boxRect.x, boxRect.y);
+        box.draw(lineG);
+
+        // Deal with possible text painting
+        if (progressBar.isStringPainted()) {
+            if (progressBar.getOrientation() == JProgressBar.HORIZONTAL) {
+                paintString(g, b.left, b.top, barRectWidth, barRectHeight,
+                        boxRect.width, b);
+            } else {
+                paintString(g, b.left, b.top, barRectWidth, barRectHeight,
+                        boxRect.height, b);
+            }
+        }
     }
 
     protected void paintDeterminate(Graphics g, JComponent c) {
