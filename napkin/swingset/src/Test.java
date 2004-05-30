79d6456d922ddcc5213e753b3457ce4548fc9fc7
kcrca <kcrca@494c9712-1f0f-0410-9e33-c2b6180fd7fb>
2004-05-30 20:48:58 +0000

    Move swingset stuff to another dir out of the (visual) way

---
+++ napkin/swingset/src/Test.java
@@ -0,0 +1,16 @@
+
+import javax.swing.*;
+
+public class Test {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("napkin.NapkinLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        JFrame lFrame = new JFrame();
+        lFrame.getContentPane().add(new JButton("blabla"));
+        lFrame.pack();
+        lFrame.show();
+    }
+}
