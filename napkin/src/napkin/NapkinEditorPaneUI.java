54ceffe70f80e0ad1a38623741dc0a0344e7ad8f
kcrca <kcrca@494c9712-1f0f-0410-9e33-c2b6180fd7fb>
2005-07-06 06:59:01 +0000

    HTML by default uses napkin font and background

--- napkin/src/napkin/NapkinEditorPaneUI.java
+++ napkin/src/napkin/NapkinEditorPaneUI.java
@@ -4,11 +4,14 @@ package napkin;
 
 import java.awt.*;
 import javax.swing.*;
+import static javax.swing.JEditorPane.*;
 import javax.swing.plaf.*;
 import javax.swing.plaf.basic.*;
 
 public class NapkinEditorPaneUI extends BasicEditorPaneUI
         implements NapkinPainter {
+    private Object origHonor;
+
     /** @noinspection MethodOverridesStaticMethodOfSuperclass */
     public static ComponentUI createUI(JComponent c) {
         return NapkinUtil.uiFor(c, new NapkinEditorPaneUI());
@@ -17,9 +20,12 @@ public class NapkinEditorPaneUI extends BasicEditorPaneUI
     public void installUI(JComponent c) {
         super.installUI(c);
         NapkinUtil.installUI(c);
+        origHonor = c.getClientProperty(HONOR_DISPLAY_PROPERTIES);
+        c.putClientProperty(HONOR_DISPLAY_PROPERTIES, Boolean.TRUE);
     }
 
     public void uninstallUI(JComponent c) {
+        c.putClientProperty(HONOR_DISPLAY_PROPERTIES, origHonor);
         NapkinUtil.uninstallUI(c);
         super.uninstallUI(c);
     }
