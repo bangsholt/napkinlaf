d938d9df975375a6f806d8b7dd5b5201c6f1c560
alexlamsl <alexlamsl@494c9712-1f0f-0410-9e33-c2b6180fd7fb>
2006-02-23 18:29:30 +0000

    When Tab is very wide, the tab will (almost) always be too small and thus a bit confusing. Fixed with adaptive range values depending on the length scale.

--- napkin/src/napkin/DrawnTabGenerator.java
+++ napkin/src/napkin/DrawnTabGenerator.java
@@ -53,20 +53,18 @@ public class DrawnTabGenerator extends DrawnShapeGenerator {
         ur = new RandomXY(1, shimmy, 0, shimmy);
         lr = new RandomXY(1, shimmy, 1, shimmy);
         ll = new RandomXY(0, shimmy, 1, shimmy);
-        if (side == LEFT || side == RIGHT) {
-            // when this gets exagerated to stretch out the line it's too much
-            double simmyAdj = shimmy / 10;
-            ul.getX().setRange(simmyAdj);
-            ur.getX().setRange(simmyAdj);
-            ll.getX().setRange(simmyAdj);
-            lr.getX().setRange(simmyAdj);
-        }
         squeeze = new RandomValue(0.09, 0.001);
     }
 
     public Shape generate(AffineTransform matrix) {
+        if (getXScale(matrix) > 100d) {
+            setScales(0.008d, 0.03d);
+        } else {
+            setScales(0.03d, 0.1d);
+        }
+        
         GeneralPath tab = new GeneralPath();
-
+        
         Point2D ulAt = ul.generate();
         Point2D urAt = ur.generate();
         Point2D llAt = ll.generate();
@@ -80,6 +78,7 @@ public class DrawnTabGenerator extends DrawnShapeGenerator {
         double xLL = llAt.getX();
         double yLL = llAt.getY();
 
+        double swap;
         switch (side) {
         case LEFT:
             yUL += squeeze.generate();
@@ -133,5 +132,28 @@ public class DrawnTabGenerator extends DrawnShapeGenerator {
 
     public RandomXY getLR() {
         return lr;
+    }
+
+    private double getXScale(AffineTransform matrix) {
+        Point2D[] points = {
+            new Point2D.Double(0d, 0d), new Point2D.Double(1d, 0d)};
+        matrix.transform(points, 0, points, 0, 2);
+        return points[0].distance(points[1]);
+    }
+    
+    private void setScales(double shimmy, double meanSqueeze) {
+        ul.getY().setRange(shimmy);
+        ur.getY().setRange(shimmy);
+        lr.getY().setRange(shimmy);
+        ll.getY().setRange(shimmy);
+        // when this gets exagerated to stretch out the line it's too much
+        if (side == LEFT || side == RIGHT)
+            shimmy /= 10d;
+        ul.getX().setRange(shimmy);
+        ur.getX().setRange(shimmy);
+        ll.getX().setRange(shimmy);
+        lr.getX().setRange(shimmy);
+        squeeze.setMid(meanSqueeze);
+        squeeze.setRange(meanSqueeze / 100);
     }
 }
