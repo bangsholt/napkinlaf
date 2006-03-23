7ee698efa4cc52946a2929399177e32b92183b65
kcrca <kcrca@494c9712-1f0f-0410-9e33-c2b6180fd7fb>
2006-03-23 14:58:12 +0000

    The 1.0 release

---
+++ tags/REL_1_0_001/trunk/napkin/src/net/sourceforge/napkinlaf/sketch/sketchers/LineSketcher.java
@@ -0,0 +1,49 @@
+// $Id$
+
+package net.sourceforge.napkinlaf.sketch.sketchers;
+
+import net.sourceforge.napkinlaf.sketch.AbstractSketcher;
+import net.sourceforge.napkinlaf.sketch.geometry.CubicLine;
+import net.sourceforge.napkinlaf.sketch.geometry.Path;
+import net.sourceforge.napkinlaf.sketch.geometry.QuadLine;
+import net.sourceforge.napkinlaf.sketch.geometry.SketchShape;
+import net.sourceforge.napkinlaf.sketch.geometry.StraightLine;
+
+/**
+ * LineSketcher: Sketches things as straight lines.
+ *
+ * @author Peter Goodspeed
+ * @author Justin Crafford
+ */
+public class LineSketcher extends AbstractSketcher {
+    /** {@inheritDoc} */
+    @Override
+    public SketchShape deformLine(StraightLine l) {
+        return l;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public SketchShape deformQuad(QuadLine q) {
+        return formPath(q.transformToLine());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public SketchShape deformCubic(CubicLine c) {
+        return formPath(c.transformToLine());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public SketchShape deformPath(Path p) {
+        Path ret = new Path();
+        SketchShape[] elements = p.simplify();
+
+        for (SketchShape element : elements) {
+            ret.append(element.deform(this), false);
+        }
+
+        return ret;
+    }
+}
---
+++ tags/REL_1_0_001/trunk/napkin/src/net/sourceforge/napkinlaf/util/NapkinTextPainter.java
@@ -0,0 +1,11 @@
+// $Id$
+
+package net.sourceforge.napkinlaf.util;
+
+import javax.swing.*;
+import java.awt.*;
+
+public interface NapkinTextPainter {
+    void superPaintText(Graphics g, JComponent c, Rectangle textRect,
+            String text);
+}
