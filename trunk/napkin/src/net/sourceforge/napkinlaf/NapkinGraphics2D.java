66852f7fe332a1988f89f9b9db17ccd0848f2abc
kcrca <kcrca@494c9712-1f0f-0410-9e33-c2b6180fd7fb>
2006-03-31 00:05:17 +0000

    Really do it with delegation

--- trunk/napkin/src/net/sourceforge/napkinlaf/NapkinGraphics2D.java
+++ trunk/napkin/src/net/sourceforge/napkinlaf/NapkinGraphics2D.java
@@ -1,58 +1,16 @@
 package net.sourceforge.napkinlaf;
 
 import java.awt.*;
+import java.awt.RenderingHints.*;
+import java.awt.font.*;
+import java.awt.geom.*;
 import java.awt.image.*;
+import java.awt.image.renderable.*;
+import java.text.AttributedCharacterIterator;
+import java.util.Map;
 
-public abstract class NapkinGraphics2D extends Graphics2D {
-    /**
-     * Draws a 3-D highlighted outline of the specified rectangle. The edges of
-     * the rectangle are highlighted so that they appear to be beveled and lit
-     * from the upper left corner.
-     * <p/>
-     * The colors used for the highlighting effect are determined based on the
-     * current color. The resulting rectangle covers an area that is
-     * <code>width&nbsp;+&nbsp;1</code> pixels wide by <code>height&nbsp;+&nbsp;1</code>
-     * pixels tall.  This method uses the current <code>Color</code> exclusively
-     * and ignores the current <code>Paint</code>.
-     *
-     * @param x      the x coordinate of the rectangle to be drawn.
-     * @param y      the y coordinate of the rectangle to be drawn.
-     * @param width  the width of the rectangle to be drawn.
-     * @param height the height of the rectangle to be drawn.
-     * @param raised a boolean that determines whether the rectangle appears to
-     *               be raised above the surface or sunk into the surface.
-     *
-     * @see Graphics#fill3DRect
-     */
-    @Override
-    public void draw3DRect(int x, int y, int width, int height,
-            boolean raised) {
-        super.draw3DRect(x, y, width, height, raised);
-    }
-
-    /**
-     * Paints a 3-D highlighted rectangle filled with the current color. The
-     * edges of the rectangle are highlighted so that it appears as if the edges
-     * were beveled and lit from the upper left corner. The colors used for the
-     * highlighting effect and for filling are determined from the current
-     * <code>Color</code>.  This method uses the current <code>Color</code>
-     * exclusively and ignores the current <code>Paint</code>.
-     *
-     * @param x      the x coordinate of the rectangle to be filled.
-     * @param y      the y coordinate of the rectangle to be filled.
-     * @param width  the width of the rectangle to be filled.
-     * @param height the height of the rectangle to be filled.
-     * @param raised a boolean value that determines whether the rectangle
-     *               appears to be raised above the surface or etched into the
-     *               surface.
-     *
-     * @see Graphics#draw3DRect
-     */
-    @Override
-    public void fill3DRect(int x, int y, int width, int height,
-            boolean raised) {
-        super.fill3DRect(x, y, width, height, raised);
-    }
+public abstract class NapkinGraphics2D {
+    private final Graphics2D graphics2D;
 
     /**
      * Constructs a new <code>Graphics2D</code> object.  Since
@@ -66,231 +24,239 @@ public abstract class NapkinGraphics2D extends Graphics2D {
      * @see Component#getGraphics
      * @see Graphics#create
      */
-    protected NapkinGraphics2D() {
-        super();
+    protected NapkinGraphics2D(Graphics2D graphics2D) {
+        this.graphics2D = graphics2D;
     }
 
-    /**
-     * Creates a new <code>Graphics</code> object based on this
-     * <code>Graphics</code> object, but with a new translation and clip area.
-     * The new <code>Graphics</code> object has its origin translated to the
-     * specified point (<i>x</i>,&nbsp;<i>y</i>). Its clip area is determined by
-     * the intersection of the original clip area with the specified rectangle.
-     * The arguments are all interpreted in the coordinate system of the
-     * original <code>Graphics</code> object. The new graphics context is
-     * identical to the original, except in two respects:
-     * <p/>
-     * <ul> <li> The new graphics context is translated by
-     * (<i>x</i>,&nbsp;<i>y</i>). That is to say, the point
-     * (<code>0</code>,&nbsp;<code>0</code>) in the new graphics context is the
-     * same as (<i>x</i>,&nbsp;<i>y</i>) in the original graphics context. <li>
-     * The new graphics context has an additional clipping rectangle, in
-     * addition to whatever (translated) clipping rectangle it inherited from
-     * the original graphics context. The origin of the new clipping rectangle
-     * is at (<code>0</code>,&nbsp;<code>0</code>), and its size is specified by
-     * the <code>width</code> and <code>height</code> arguments. </ul>
-     * <p/>
-     *
-     * @param x      the <i>x</i> coordinate.
-     * @param y      the <i>y</i> coordinate.
-     * @param width  the width of the clipping rectangle.
-     * @param height the height of the clipping rectangle.
-     *
-     * @return a new graphics context.
-     *
-     * @see Graphics#translate
-     * @see Graphics#clipRect
-     */
-    @Override
+    public abstract void draw(Shape s);
+
+    public abstract boolean drawImage(Image img,
+                                      AffineTransform xform,
+                                      ImageObserver obs);
+
+    public abstract void drawImage(BufferedImage img,
+                                   BufferedImageOp op,
+                                   int x,
+                                   int y);
+
+    public abstract void drawRenderedImage(RenderedImage img,
+                                           AffineTransform xform);
+
+    public abstract void drawRenderableImage(RenderableImage img,
+                                             AffineTransform xform);
+
+    public abstract void drawString(String str, int x, int y);
+
+    public abstract void drawString(String s, float x, float y);
+
+    public abstract void drawString(AttributedCharacterIterator iterator,
+                                    int x, int y);
+
+    public abstract void drawString(AttributedCharacterIterator iterator,
+                                    float x, float y);
+
+    public abstract void drawGlyphVector(GlyphVector g, float x, float y);
+
+    public abstract void fill(Shape s);
+
+    public abstract boolean hit(Rectangle rect,
+                                Shape s,
+                                boolean onStroke);
+
+    public abstract GraphicsConfiguration getDeviceConfiguration();
+
+    public abstract void setComposite(Composite comp);
+
+    public abstract void setPaint( Paint paint );
+
+    public abstract void setStroke(Stroke s);
+
+    public abstract void setRenderingHint(Key hintKey, Object hintValue);
+
+    public abstract Object getRenderingHint(Key hintKey);
+
+    public abstract void setRenderingHints(Map<?,?> hints);
+
+    public abstract void addRenderingHints(Map<?,?> hints);
+
+    public abstract RenderingHints getRenderingHints();
+
+    public abstract void translate(int x, int y);
+
+    public abstract void translate(double tx, double ty);
+
+    public abstract void rotate(double theta);
+
+    public abstract void rotate(double theta, double x, double y);
+
+    public abstract void scale(double sx, double sy);
+
+    public abstract void shear(double shx, double shy);
+
+    public abstract void transform(AffineTransform Tx);
+
+    public abstract void setTransform(AffineTransform Tx);
+
+    public abstract AffineTransform getTransform();
+
+    public abstract Paint getPaint();
+
+    public abstract Composite getComposite();
+
+    public abstract void setBackground(Color color);
+
+    public abstract Color getBackground();
+
+    public abstract Stroke getStroke();
+
+    public abstract void clip(Shape s);
+
+    public abstract FontRenderContext getFontRenderContext();
+
+    public abstract Graphics create();
+
+    public abstract Color getColor();
+
+    public abstract void setColor(Color c);
+
+    public abstract void setPaintMode();
+
+    public abstract void setXORMode(Color c1);
+
+    public abstract Font getFont();
+
+    public abstract void setFont(Font font);
+
+    public abstract FontMetrics getFontMetrics(Font f);
+
+    public abstract Rectangle getClipBounds();
+
+    public abstract void clipRect(int x, int y, int width, int height);
+
+    public abstract void setClip(int x, int y, int width, int height);
+
+    public abstract Shape getClip();
+
+    public abstract void setClip(Shape clip);
+
+    public abstract void copyArea(int x, int y, int width, int height,
+                                  int dx, int dy);
+
+    public abstract void drawLine(int x1, int y1, int x2, int y2);
+
+    public abstract void fillRect(int x, int y, int width, int height);
+
+    public abstract void clearRect(int x, int y, int width, int height);
+
+    public abstract void drawRoundRect(int x, int y, int width, int height,
+                                       int arcWidth, int arcHeight);
+
+    public abstract void fillRoundRect(int x, int y, int width, int height,
+                                       int arcWidth, int arcHeight);
+
+    public abstract void drawOval(int x, int y, int width, int height);
+
+    public abstract void fillOval(int x, int y, int width, int height);
+
+    public abstract void drawArc(int x, int y, int width, int height,
+                                 int startAngle, int arcAngle);
+
+    public abstract void fillArc(int x, int y, int width, int height,
+                                 int startAngle, int arcAngle);
+
+    public abstract void drawPolyline(int xPoints[], int yPoints[],
+                                      int nPoints);
+
+    public abstract void drawPolygon(int xPoints[], int yPoints[],
+                                     int nPoints);
+
+    public abstract void fillPolygon(int xPoints[], int yPoints[],
+                                     int nPoints);
+
+    public abstract boolean drawImage(Image img, int x, int y,
+                                      ImageObserver observer);
+
+    public abstract boolean drawImage(Image img, int x, int y,
+                                      int width, int height,
+                                      ImageObserver observer);
+
+    public abstract boolean drawImage(Image img, int x, int y,
+                                      Color bgcolor,
+                                      ImageObserver observer);
+
+    public abstract boolean drawImage(Image img, int x, int y,
+                                      int width, int height,
+                                      Color bgcolor,
+                                      ImageObserver observer);
+
+    public abstract boolean drawImage(Image img,
+                                      int dx1, int dy1, int dx2, int dy2,
+                                      int sx1, int sy1, int sx2, int sy2,
+                                      ImageObserver observer);
+
+    public abstract boolean drawImage(Image img,
+                                      int dx1, int dy1, int dx2, int dy2,
+                                      int sx1, int sy1, int sx2, int sy2,
+                                      Color bgcolor,
+                                      ImageObserver observer);
+
+    public abstract void dispose();
+
+    public Graphics2D getGraphics2D() {
+        return graphics2D;
+    }
+
     public Graphics create(int x, int y, int width, int height) {
-        return super.create(x, y, width, height);
+        return graphics2D.create(x, y, width, height);
     }
 
-    /**
-     * Draws the text given by the specified byte array, using this graphics
-     * context's current font and color. The baseline of the first character is
-     * at position (<i>x</i>,&nbsp;<i>y</i>) in this graphics context's
-     * coordinate system.
-     *
-     * @param data   the data to be drawn
-     * @param offset the start offset in the data
-     * @param length the number of bytes that are drawn
-     * @param x      the <i>x</i> coordinate of the baseline of the text
-     * @param y      the <i>y</i> coordinate of the baseline of the text
-     *
-     * @see Graphics#drawChars
-     * @see Graphics#drawString
-     */
-    @Override
-    public void drawBytes(byte data[], int offset, int length, int x, int y) {
-        super.drawBytes(data, offset, length, x, y);
-    }
-
-    /**
-     * Draws the text given by the specified character array, using this
-     * graphics context's current font and color. The baseline of the first
-     * character is at position (<i>x</i>,&nbsp;<i>y</i>) in this graphics
-     * context's coordinate system.
-     *
-     * @param data   the array of characters to be drawn
-     * @param offset the start offset in the data
-     * @param length the number of characters to be drawn
-     * @param x      the <i>x</i> coordinate of the baseline of the text
-     * @param y      the <i>y</i> coordinate of the baseline of the text
-     *
-     * @see Graphics#drawBytes
-     * @see Graphics#drawString
-     */
-    @Override
-    public void drawChars(char data[], int offset, int length, int x, int y) {
-        super.drawChars(data, offset, length, x, y);
-    }
-
-    /**
-     * Draws the outline of a polygon defined by the specified
-     * <code>Polygon</code> object.
-     *
-     * @param p the polygon to draw.
-     *
-     * @see Graphics#fillPolygon
-     * @see Graphics#drawPolyline
-     */
-    @Override
-    public void drawPolygon(Polygon p) {
-        super.drawPolygon(p);
-    }
-
-    /**
-     * Draws the outline of the specified rectangle. The left and right edges of
-     * the rectangle are at <code>x</code> and <code>x&nbsp;+&nbsp;width</code>.
-     * The top and bottom edges are at <code>y</code> and
-     * <code>y&nbsp;+&nbsp;height</code>. The rectangle is drawn using the
-     * graphics context's current color.
-     *
-     * @param x      the <i>x</i> coordinate of the rectangle to be drawn.
-     * @param y      the <i>y</i> coordinate of the rectangle to be drawn.
-     * @param width  the width of the rectangle to be drawn.
-     * @param height the height of the rectangle to be drawn.
-     *
-     * @see Graphics#fillRect
-     * @see Graphics#clearRect
-     */
-    @Override
-    public void drawRect(int x, int y, int width, int height) {
-        super.drawRect(x, y, width, height);
-    }
-
-    /**
-     * Fills the polygon defined by the specified Polygon object with the
-     * graphics context's current color.
-     * <p/>
-     * The area inside the polygon is defined using an even-odd fill rule, also
-     * known as the alternating rule.
-     *
-     * @param p the polygon to fill.
-     *
-     * @see Graphics#drawPolygon(int[], int[], int)
-     */
-    @Override
-    public void fillPolygon(Polygon p) {
-        super.fillPolygon(p);
-    }
-
-    /**
-     * Disposes of this graphics context once it is no longer referenced.
-     *
-     * @see #dispose
-     */
-    @Override
-    public void finalize() {
-        super.finalize();
-    }
-
-    /**
-     * Returns the bounding rectangle of the current clipping area. The
-     * coordinates in the rectangle are relative to the coordinate system origin
-     * of this graphics context.  This method differs from {@link
-     * #getClipBounds() getClipBounds} in that an existing rectangle is used
-     * instead of allocating a new one. This method refers to the user clip,
-     * which is independent of the clipping associated with device bounds and
-     * window visibility. If no clip has previously been set, or if the clip has
-     * been cleared using <code>setClip(null)</code>, this method returns the
-     * specified <code>Rectangle</code>.
-     *
-     * @param r the rectangle where the current clipping area is copied to.  Any
-     *          current values in this rectangle are overwritten.
-     *
-     * @return the bounding rectangle of the current clipping area.
-     */
-    @Override
-    public Rectangle getClipBounds(Rectangle r) {
-        return super.getClipBounds(r);
-    }
-
-    /**
-     * Returns the bounding rectangle of the current clipping area.
-     *
-     * @return the bounding rectangle of the current clipping area or
-     *         <code>null</code> if no clip is set.
-     *
-     * @deprecated As of JDK version 1.1, replaced by <code>getClipBounds()</code>.
-     */
-    @Override
-    @Deprecated
-    public Rectangle getClipRect() {
-        return super.getClipRect();
-    }
-
-    /**
-     * Gets the font metrics of the current font.
-     *
-     * @return the font metrics of this graphics context's current font.
-     *
-     * @see Graphics#getFont
-     * @see FontMetrics
-     * @see Graphics#getFontMetrics(Font)
-     */
-    @Override
     public FontMetrics getFontMetrics() {
-        return super.getFontMetrics();
+        return graphics2D.getFontMetrics();
     }
 
-    /**
-     * Returns true if the specified rectangular area might intersect the
-     * current clipping area. The coordinates of the specified rectangular area
-     * are in the user coordinate space and are relative to the coordinate
-     * system origin of this graphics context. This method may use an algorithm
-     * that calculates a result quickly but which sometimes might return true
-     * even if the specified rectangular area does not intersect the clipping
-     * area. The specific algorithm employed may thus trade off accuracy for
-     * speed, but it will never return false unless it can guarantee that the
-     * specified rectangular area does not intersect the current clipping area.
-     * The clipping area used by this method can represent the intersection of
-     * the user clip as specified through the clip methods of this graphics
-     * context as well as the clipping associated with the device or image
-     * bounds and window visibility.
-     *
-     * @param x      the x coordinate of the rectangle to test against the clip
-     * @param y      the y coordinate of the rectangle to test against the clip
-     * @param width  the width of the rectangle to test against the clip
-     * @param height the height of the rectangle to test against the clip
-     *
-     * @return <code>true</code> if the specified rectangle intersects the
-     *         bounds of the current clip; <code>false</code> otherwise.
-     */
-    @Override
-    public boolean hitClip(int x, int y, int width, int height) {
-        return super.hitClip(x, y, width, height);
+    public void drawRect(int x, int y, int width, int height) {
+        graphics2D.drawRect(x, y, width, height);
     }
 
-    /**
-     * Returns a <code>String</code> object representing this
-     * <code>Graphics</code> object's value.
-     *
-     * @return a string representation of this graphics context.
-     */
-    @Override
+    public void drawPolygon(Polygon p) {
+        graphics2D.drawPolygon(p);
+    }
+
+    public void fillPolygon(Polygon p) {
+        graphics2D.fillPolygon(p);
+    }
+
+    public void drawChars(char[] data, int offset, int length, int x, int y) {
+        graphics2D.drawChars(data, offset, length, x, y);
+    }
+
+    public void drawBytes(byte[] data, int offset, int length, int x, int y) {
+        graphics2D.drawBytes(data, offset, length, x, y);
+    }
+
     public String toString() {
-        return super.toString();
+        return graphics2D.toString();
+    }
+
+    public Rectangle getClipRect() {
+        return graphics2D.getClipRect();
+    }
+
+    public boolean hitClip(int x, int y, int width, int height) {
+        return graphics2D.hitClip(x, y, width, height);
+    }
+
+    public Rectangle getClipBounds(Rectangle r) {
+        return graphics2D.getClipBounds(r);
+    }
+
+    public void draw3DRect(int x, int y, int width, int height,
+            boolean raised) {
+        graphics2D.draw3DRect(x, y, width, height, raised);
+    }
+
+    public void fill3DRect(int x, int y, int width, int height,
+            boolean raised) {
+        graphics2D.fill3DRect(x, y, width, height, raised);
     }
 }
