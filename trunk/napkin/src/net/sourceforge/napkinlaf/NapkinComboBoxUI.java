0ba9d3cfc2dca884cdd7e8cf6d4c96aa030cf125
alexlamsl <alexlamsl@494c9712-1f0f-0410-9e33-c2b6180fd7fb>
2006-04-03 23:04:58 +0000

    a better fix of the ComboBox's list with better docs as well

--- trunk/napkin/src/net/sourceforge/napkinlaf/NapkinComboBoxUI.java
+++ trunk/napkin/src/net/sourceforge/napkinlaf/NapkinComboBoxUI.java
@@ -1,7 +1,13 @@
 package net.sourceforge.napkinlaf;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import javax.swing.event.ListDataListener;
 import static net.sourceforge.napkinlaf.util.NapkinConstants.SOUTH;
 import net.sourceforge.napkinlaf.util.NapkinPainter;
 import net.sourceforge.napkinlaf.util.NapkinUtil;
@@ -14,28 +20,32 @@ import java.awt.*;
 public class NapkinComboBoxUI extends BasicComboBoxUI
         implements NapkinPainter {
 
+    /**!!
+     * Due to "GES, 980818" in BasicComboBoxUI, which seems to be a workaround
+     * for java bug 4168483, we need to get set our popup to non-light-weight
+     * in order for the workaround's setPopupVisible(false) not to fire, which
+     * results in the popup menu disappearing upon focusGained().
+     *
+     * The "default" behaviour causes scroll bars to be seemed as non-functional
+     */
+    private boolean wasLightWeightPopupEnabled;
+
     @SuppressWarnings({"UnusedParameters"})
     public static ComponentUI createUI(JComponent c) {
         return new NapkinComboBoxUI();
-    }
-
-    /**
-     * The default focus listener does not behave correctly.
-     * And the solution is to simply get rid of it.
-     */
-    @Override
-    protected FocusListener createFocusListener() {
-        return null;
     }
 
     @Override
     public void installUI(JComponent c) {
         super.installUI(c);
         NapkinUtil.installUI(c);
+        wasLightWeightPopupEnabled = comboBox.isLightWeightPopupEnabled();
+        comboBox.setLightWeightPopupEnabled(false);
     }
 
     @Override
     public void uninstallUI(JComponent c) {
+        comboBox.setLightWeightPopupEnabled(wasLightWeightPopupEnabled);
         NapkinUtil.uninstallUI(c);
         super.uninstallUI(c);
     }
@@ -48,7 +58,6 @@ public class NapkinComboBoxUI extends BasicComboBoxUI
     @Override
     public void paintCurrentValueBackground(Graphics g, Rectangle bounds,
             boolean hasFocus) {
-
         // we don't want any special background
     }
 
