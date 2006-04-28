b200a2365ddd97528085c62fa760e78499313a90
alexlamsl <alexlamsl@494c9712-1f0f-0410-9e33-c2b6180fd7fb>
2006-04-28 16:33:43 +0000

    got some nice edge icons now...

--- napkin/core/src/main/java/net/sourceforge/napkinlaf/sketch/SketchedImageIcon.java
+++ napkin/core/src/main/java/net/sourceforge/napkinlaf/sketch/SketchedImageIcon.java
@@ -12,8 +12,7 @@ package net.sourceforge.napkinlaf.sketch;
 import net.sourceforge.napkinlaf.util.NapkinIcon;
 
 import java.awt.*;
-import java.awt.image.BandCombineOp;
-import java.awt.image.BufferedImage;
+import java.awt.image.*;
 import javax.swing.*;
 import static java.awt.RenderingHints.*;
 
@@ -23,30 +22,96 @@ import static java.awt.RenderingHints.*;
  */
 public class SketchedImageIcon extends ImageIcon implements NapkinIcon {
     
+    private static final RenderingHints hints = new RenderingHints(null);
+    static {
+        hints.put(KEY_ALPHA_INTERPOLATION, VALUE_ALPHA_INTERPOLATION_QUALITY);
+        hints.put(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
+        hints.put(KEY_INTERPOLATION, VALUE_INTERPOLATION_BICUBIC);
+    }
+
+    private static final BandCombineOp desaturateOp = new BandCombineOp(
+            new float[][] {
+                {0.299f, 0.587f, 0.114f,     0f},
+            }, hints
+        );
+
+    private static final BandCombineOp invertOp = new BandCombineOp(
+            new float[][] {
+                {-1f,  1f},
+            }, hints
+        );
+    
+    private static final ConvolveOp blurOp = new ConvolveOp(
+            new Kernel(3, 3,
+                new float[] {
+                    0.0625f, 0.1250f, 0.0625f,
+                    0.1250f, 0.2500f, 0.1250f,
+                    0.0625f, 0.1250f, 0.0625f,
+                }
+            ),
+            ConvolveOp.EDGE_NO_OP, hints
+        );
+
+    private static final ConvolveOp sharpenOp = new ConvolveOp(
+            new Kernel(3, 3,
+                new float[] {
+                    -1f, -1f, -1f,
+                    -1f,  9f, -1f,
+                    -1f, -1f, -1f,
+                }
+            ),
+            ConvolveOp.EDGE_NO_OP, hints
+        );
+
     /** Creates a new instance of SketchedImageIcon */
     public SketchedImageIcon(JComponent component, Icon icon) {
         super(sketchIcon(component, icon));
     }
     
     private static Image sketchIcon(JComponent component, Icon icon) {
-        BufferedImage image = new BufferedImage(icon.getIconWidth(),
-                icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+        int width = icon.getIconWidth();
+        int height = icon.getIconHeight();
+        BufferedImage image =
+                new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         Graphics g = image.getGraphics();
+        ((Graphics2D) g).setRenderingHints(hints);
         icon.paintIcon(component, g, 0, 0);
-        RenderingHints hints = new RenderingHints(null);
-        hints.put(KEY_ALPHA_INTERPOLATION, VALUE_ALPHA_INTERPOLATION_QUALITY);
-        hints.put(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
-        hints.put(KEY_INTERPOLATION, VALUE_INTERPOLATION_BICUBIC);
-        BandCombineOp bandOp = new BandCombineOp(
-                new float[][] {
-                    {0.299f, 0.587f, 0.114f, 0f},
-                    {0.299f, 0.587f, 0.114f, 0f},
-                    {0.299f, 0.587f, 0.114f, 0f},
-                    {0f, 0f, 0f, 1f},
-                }, null
-            );
-        image.setData(bandOp.filter(image.getData(), null));
-        return image;
+        // create a desaturated image
+        BufferedImage grayImage =
+                new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        desaturateOp.filter(image.getRaster(), grayImage.getRaster());
+        // invert and blur the desaturated image
+        BufferedImage grayInvertedImage =
+                new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        invertOp.filter(grayImage.getRaster(), grayInvertedImage.getRaster());
+        grayInvertedImage
+                .setData(blurOp.filter(grayInvertedImage.getRaster(), null));
+        // create an edge image out of the 2 layers
+        BufferedImage edgeImage =
+                new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        WritableRaster gRaster = grayImage.getRaster();
+        WritableRaster gIRaster = grayInvertedImage.getRaster();
+        WritableRaster aRaster = image.getAlphaRaster();
+        WritableRaster dstRaster = edgeImage.getRaster();
+        int[] src1 = new int[1];
+        int[] src2 = new int[1];
+        int[] alpha = new int[1];
+        int[] clear = new int[]{0xFF, 0xFF, 0xFF, 0x00};
+        int[] black = new int[]{0x00, 0x00, 0x00, 0xFF};
+        for (int x, y = 0; y < height; y++) {
+            for (x = 0; x < width; x++) {
+                gRaster.getPixel(x, y, src1);
+                gIRaster.getPixel(x, y, src2);
+                aRaster.getPixel(x, y, alpha);
+                dstRaster.setPixel(x, y,
+                        alpha[0] > 0x20 && src1[0] + src2[0] < 0xF0 ?
+                            black : clear);
+            }
+        }
+        // overlay the edge image onto the sharpened original
+        image.setData(sharpenOp.filter(image.getRaster(), null));
+        g.drawImage(edgeImage, 0, 0, null);
+        return edgeImage;
     }
     
 }
