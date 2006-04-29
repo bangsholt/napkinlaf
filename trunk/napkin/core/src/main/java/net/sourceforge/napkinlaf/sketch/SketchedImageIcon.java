86a15cf64225427218c24222c042591b5a1788ad
alexlamsl <alexlamsl@494c9712-1f0f-0410-9e33-c2b6180fd7fb>
2006-04-29 12:38:19 +0000

    pen(cil) sketch edition ;-)

--- trunk/napkin/core/src/main/java/net/sourceforge/napkinlaf/sketch/SketchedImageIcon.java
+++ trunk/napkin/core/src/main/java/net/sourceforge/napkinlaf/sketch/SketchedImageIcon.java
@@ -41,7 +41,7 @@ public class SketchedImageIcon extends ImageIcon implements NapkinIcon {
             }, hints
         );
     
-    private static final ConvolveOp blurOp = new ConvolveOp(
+    private static final ConvolveOp bigBlurOp = new ConvolveOp(
             new Kernel(3, 3,
                 new float[] {
                     0.0625f, 0.1250f, 0.0625f,
@@ -52,12 +52,23 @@ public class SketchedImageIcon extends ImageIcon implements NapkinIcon {
             ConvolveOp.EDGE_NO_OP, hints
         );
 
+    private static final ConvolveOp smallBlurOp = new ConvolveOp(
+            new Kernel(3, 3,
+                new float[] {
+                    0.05f, 0.10f, 0.05f,
+                    0.10f, 0.40f, 0.10f,
+                    0.05f, 0.10f, 0.05f,
+                }
+            ),
+            ConvolveOp.EDGE_NO_OP, hints
+        );
+
     private static final ConvolveOp sharpenOp = new ConvolveOp(
             new Kernel(3, 3,
                 new float[] {
-                    -1f, -1f, -1f,
-                    -1f,  9f, -1f,
-                    -1f, -1f, -1f,
+                    -0.5f, -0.5f, -0.5f,
+                    -0.5f,  5.0f, -0.5f,
+                    -0.5f, -0.5f, -0.5f,
                 }
             ),
             ConvolveOp.EDGE_NO_OP, hints
@@ -85,7 +96,7 @@ public class SketchedImageIcon extends ImageIcon implements NapkinIcon {
                 new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
         invertOp.filter(grayImage.getRaster(), grayInvertedImage.getRaster());
         grayInvertedImage
-                .setData(blurOp.filter(grayInvertedImage.getRaster(), null));
+                .setData(bigBlurOp.filter(grayInvertedImage.getRaster(), null));
         // create an edge image out of the 2 layers
         BufferedImage edgeImage =
                 new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
@@ -96,24 +107,27 @@ public class SketchedImageIcon extends ImageIcon implements NapkinIcon {
         int[] src1 = new int[1];
         int[] src2 = new int[1];
         int[] alpha = new int[1];
-        int[] color = new int[4];
+        int[] color = new int[] {0x00, 0x00, 0x00, 0x00};
+        double c;
         for (int x, y = 0; y < height; y++) {
             for (x = 0; x < width; x++) {
                 gRaster.getPixel(x, y, src1);
                 gIRaster.getPixel(x, y, src2);
                 aRaster.getPixel(x, y, alpha);
-                color[0] = src1[0] + src2[0] < 0xF0 ? 0x00 : 0xFF;
-                color[2] = color[1] = color[0];
-                color[3] = (color[0] == 0x00) ? alpha[0] : 0x00;
+                c = (src1[0] + src2[0]) / 255d;
+                c = Math.max(0d, Math.min(1d, (c - 0.75d) / 0.25d));
+                c = Math.pow(c, 4d);
+                color[3] = (int) (alpha[0] * (1d - c));
                 dstRaster.setPixel(x, y, color);
             }
         }
         // smoothen the edges
-        edgeImage.setData(blurOp.filter(edgeImage.getRaster(), null));
-        edgeImage.setData(sharpenOp.filter(edgeImage.getRaster(), null));
+//        edgeImage.setData(smallBlurOp.filter(edgeImage.getRaster(), null));
+//        edgeImage.setData(sharpenOp.filter(edgeImage.getRaster(), null));
+        return edgeImage;
         // overlay the edge image onto the original
-        g.drawImage(edgeImage, 0, 0, null);
-        return image;
+//        g.drawImage(edgeImage, 0, 0, null);
+//        return image;
     }
     
 }
